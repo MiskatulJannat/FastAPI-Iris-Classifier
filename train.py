c1c793742b498550748146d0ec15e73d886da187
Miskatul Jannat <miskatulj3@gmail.com>
2024-05-17 19:43:31 +0600

    modified:   train.py

--- train.py
+++ train.py
@@ -11,10 +11,10 @@ y = iris.target
 # Split the dataset
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-clf = RandomForestClassifier(n_jobs=-1)
-clf.fit(x_train, y_train)
-print(clf.score(x_test, y_test))
+the_model = RandomForestClassifier(n_jobs=-1)
+the_model.fit(x_train, y_train)
+print(the_model.score(x_test, y_test))
 
 # Save the model
 with open('iris_model.pkl', 'wb') as f:
-    pickle.dump(clf, f)
+    pickle.dump(the_model, f)
